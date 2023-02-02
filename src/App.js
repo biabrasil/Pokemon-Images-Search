40021283fbe2fa9894aef0847a98f3faa2f0cb49
biabrasil <bbia20@outlook.pt>
2023-02-02 16:08:36 +0000

    Adding Github banner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
-import ProfileCard from "./ProfileCard";
+import ProfileCard from "./components/ProfileCard";
 import Bulbasaur from "./images/bulbasaur.webp";
 import Chikorita from "./images/chikorita.png";
 import Pikachu from "./images/pikachu.png";
 import Pokédex from "./images/pokedex.webp";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner></Banner>
       <div className="flex pt-5 pb-10 justify-center">
         <img className="w-44" src={Pokédex} />
       </div>
